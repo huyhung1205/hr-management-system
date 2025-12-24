bda72f8f4c9feaeeeae370cb8b83b610eb1852d1
Tran Hai Thien <tranhaithien2000@gmail.com>
2025-12-24 22:01:00 +0700

    update-statistical

--- human_resource_management/human_resource_management/Areas/HumanResource/Controllers/HomeController.cs
+++ human_resource_management/human_resource_management/Areas/HumanResource/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using human_resource_management.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,7 +24,15 @@ namespace human_resource_management.Areas.HumanResource.Controllers
         }
         public ActionResult Statistical()
         {
-            return View();
+            // Sử dụng 'using' để quản lý bộ nhớ tốt nhất
+            using (ModelDBContext db = new ModelDBContext())
+            {
+                // Thêm .Include("NhanViens") để tải luôn danh sách nhân viên đi kèm phòng ban
+                // Giúp đếm số lượng chính xác ngay lập tức (Eager Loading)
+                var data = db.PhongBans.Include("NhanViens").ToList();
+
+                return View(data);
+            }
         }
 
     }
